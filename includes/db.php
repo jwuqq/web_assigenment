8879b0d220519f9eed21d4eaf5e1cdecbf45445c
王昆 <24160144@example.com>
2026-07-10 18:24:28 +0800

    fix: 数据库连接加utf8mb4编码防止中文乱码

--- includes/db.php
+++ includes/db.php
@@ -43,3 +43,5 @@ $conn = new mysqli($db_host, $db_username, $db_password, $db_name, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$conn->set_charset('utf8mb4');
